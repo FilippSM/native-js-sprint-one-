b9bc7343ab6fd07e9673524d012260f8b4517e8d
Filipp Sam <philippsm666@gmail.com>
2025-01-09 00:51:17 +0300

    (sprint 2 week 2) homework copy deep finish

--- src/homework/homework.test.tsx
+++ src/homework/homework.test.tsx
@@ -160,9 +160,55 @@ test('Object inside an object -> array -> object ->  object', () => {
         }
     };
 
-    let man6FullCopy = {...man6, mother: {...man6.mother, parents: man6.mother.parents.map(el => ({...el}))}}
-    man5FullCopy.mother.parents[0].age = 91;
+    let man6FullCopy = {...man6, mother: {...man6.mother, parents: man6.mother.parents.map(el => ({...el, favoriteDish: {...el.favoriteDish}}))}}
+    man6FullCopy.mother.parents[0].age = 55;
     
-    expect(man5.mother.parents[0].age).toBe(80)
-    expect(man5FullCopy.mother.parents[0].age).toBe(91)
+    expect( man6.mother.parents[0].age).toBe(80)
+    expect( man6FullCopy.mother.parents[0].age).toBe(55)
+})
+
+test('Array of objects inside an object -> object -> array -> object ->  object', () => {
+    let man7 = {
+        name: 'John',
+        age: 28,
+        mother: {
+            name: "Kate",
+            age: 50,
+            work: {
+                position: "doctor",
+                experience: 15
+            },
+            parents: [
+                {
+                    name: "Kevin",
+                    age: 80,
+                    favoriteDish: {
+                        title: "borscht",
+                        ingredients: [
+                            {title: "beet", amount: 3},
+                            {title: "potatoes", amount: 5},
+                            {title: "carrot", amount: 1},
+                        ]
+                    }
+                },
+                {
+                    name: "Jennifer",
+                    age: 78,
+                    favoriteDish: {
+                        title: "sushi",
+                        ingredients: [
+                            {title: "fish", amount: 1},
+                            {title: "rise", amount: 0.5}
+                        ]
+                    }
+                },
+            ]
+        }
+    };
+
+    let man7FullCopy = {...man7, mother: {...man7.mother, parents: man7.mother.parents.map(el => ({...el, favoriteDish: {...el.favoriteDish, ingredients: el.favoriteDish.ingredients.map(el => ({...el}))}}))}}
+    man7FullCopy.mother.parents[0].favoriteDish.ingredients[0].title = "sup";
+    
+    expect( man7.mother.parents[0].favoriteDish.ingredients[0].title).toBe("beet")
+    expect( man7FullCopy.mother.parents[0].favoriteDish.ingredients[0].title).toBe("sup")
 })
